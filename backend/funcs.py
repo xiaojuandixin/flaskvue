94c570153cd62199b67b790fbd5811ca822b4fe2
DixinFan <dixinfan@foxmail.com>
2022-12-19 12:42:46 +0000

    set token exp 3mins

--- backend/funcs.py
+++ backend/funcs.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import sqlite3
 import jwt
 from query_db import *
+import datetime
 app = Flask(__name__)
 
 # 1. login
@@ -9,26 +10,20 @@ app = Flask(__name__)
 # {"code":200,"msg":"Password is correct"}
 @app.route("/api/login", methods=["POST"])
 def login():
-    response = {}
     # Retrieve the user data from the request
-    data = request.get_json()
-    username = data["username"]
-    password = data["password"]
+    params = request.get_json()
+    username = params["username"]
+    password = params["password"]
 
     # # Query the database to check if the user exists
-    # conn = sqlite3.connect("pfm.db")
-    # cursor = conn.cursor()
-    # cursor.execute("SELECT * FROM users WHERE name=?", (username,))
-    # user = cursor.fetchone()
-    # conn.close()
     sql = "SELECT * FROM users WHERE name='"+username+"';"
     user = query(sql)
 
+    response = {}
     # If the user doesn't exist, return a 404 error
     if not user:
         response['msg'] =  "User not found"
         response['code'] = 404
-        response['token'] = ''
         return response
 
     # If the user exists, check if their password is correct
@@ -36,18 +31,26 @@ def login():
     if db_password == password:
         response['msg'] =  "Password is correct"
         response['code'] = 200
-        response['token'] = generate_token(user[0], user[1])
+
+        uid, name = user[0], user[1]
+
+        sql = "SELECT * FROM roles WHERE uid="+str(uid)+";"
+        role = query(sql)
+
+        # 设置 JWT 令牌的有效时间为 3 分钟
+        expiry_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=3)
+        
+        data = {'uid': uid, 'name': name, 'role': role[2], "exp": expiry_time}
+        response['data'] = data
+
+        response['token'] = generate_token(data)
         return response
     else:
         response['msg'] =  "Password is incorrect"
         response['code'] = 401
-        response['token'] = ''
         return response
 
 
-def generate_token(uid, name):
-    sql = "SELECT * FROM roles WHERE uid="+str(uid)+";"
-    role = query(sql)
-    data = {'uid': uid, 'name': name, 'role': role[2]}
+def generate_token(data):
     secret = 'secret-key'
     return jwt.encode(data, secret, algorithm='HS256')
